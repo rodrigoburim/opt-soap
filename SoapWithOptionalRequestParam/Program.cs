2f7bcb539bac35820f9a35c34479b42f500c7317
Rodrigo Burim <rodrigoburim@gmail.com>
2016-11-14 15:09:27 -0200

    init

---
+++ SoapWithOptionalRequestParam/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoapWithOptionalRequestParam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string xml = @"<soapenv:Envelope xmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/"">
+               <soapenv:Header/>
+               <soapenv:Body>
+               </soapenv:Body>
+            </soapenv:Envelope>";
+
+            SoapService service = new SoapService();
+            Console.WriteLine(service.Execute(xml, "http://url.com/wservice"));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SoapWithOptionalRequestParam/SoapService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace SoapWithOptionalRequestParam
+{
+    class SoapService
+    {
+        public string Execute(string xml, string url)
+        {
+            HttpWebRequest request = SoapRequest(url);
+            XmlDocument envelope = new XmlDocument();
+
+            envelope.LoadXml(xml);
+
+            using (Stream stream = request.GetRequestStream())
+            {
+                envelope.Save(stream);
+            }
+
+            using (WebResponse response = request.GetResponse())
+            {
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        //TODO: create a model? for header, content-type, accept and method @rburim
+        public HttpWebRequest SoapRequest(string url)
+        {
+	        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url); 
+	        request.Headers.Add(@"SOAP:Action"); 
+	        request.ContentType = "text/xml;charset=\"utf-8\""; 
+	        request.Accept = "text/xml"; 
+	        request.Method = "POST"; 
+	        return request; 
+        }
+    }
+}
